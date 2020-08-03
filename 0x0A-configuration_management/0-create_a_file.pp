0d8b2c3832a19cc265a3ccdb6d1fe2bda6478faf
danielj32 <danieljuanito21@hotmail.com>
2020-08-03 17:54:30 +0000

    add new directory and new task Using Puppet, create a file in /tmp. Using Puppet, install puppet-lint

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file using Puppet in /tmp
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
