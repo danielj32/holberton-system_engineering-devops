51e711661179bad47341329b5e2ebc6ec107f031
danielj32 <danieljuanito21@hotmail.com>
2020-08-05 02:11:19 +0000

    fix add new changes Password Authentication

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,9 @@
 # Create ssh for client with puppet
-file {'~/.ssh/config':
-  ensure  => present,
-  replace => 'yes',
-  path    => '~/.ssh/config',
-  content => 'Host *
-     HostName 34.73.23.33
-     User Daniel
-     IdentityFile ~/.ssh/holberton',
-  mode    => '7000',
+file { '/etc/ssh/ssh_config':
+    ensure  => file,
+    content => 'Host holb_server
+    HostName 34.73.23.33
+    User Daniel
+    IdentityFile ~/.ssh/holberton
+    PasswordAuthentication no',
 }
