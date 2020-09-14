d0a65b9261268ad0ba7f5dd2e874384df6853220
danielj32 <danieljuanito21@hotmail.com>
2020-09-14 23:56:06 +0000

    add new task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" tasks that are owned by this employee """
+from requests import get
+from sys import argv
+import csv
+
+
+def record(input):
+    """record of task """
+    user = get('https://jsonplaceholder.typicode.com/users/' + input).json()
+    if not user:
+        return
+    all_tasks = get('https://jsonplaceholder.typicode.com/todos/').json()
+    emp_id = user.get('id')
+    emp_username = user.get('username')
+    tasks = [t for t in all_tasks if t['userId'] == emp_id]
+    to_csv = [{"id": emp_id,
+               "name": emp_username,
+               "completed": t.get('completed'),
+               "title": t.get('title')} for t in tasks]
+    keys = ['id', 'name', 'completed', 'title']
+    with open("{}.csv".format(emp_id), "w") as f:
+        dict_writer = csv.DictWriter(f, keys, quoting=csv.QUOTE_ALL)
+        dict_writer.writerows(to_csv)
+
+
+if __name__ == '__main__':
+    if len(argv) == 2 and argv[1].isdigit():
+        record_tasks(argv[1])
